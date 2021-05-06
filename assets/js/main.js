2574f07810bea0fa95e7bc875bda9e85788499fa
Delino <64547069+d3lino@users.noreply.github.com>
2021-05-06 14:57:43 +0100

    Debut de l'algo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 const keybord = document.querySelectorAll('button[data-type="letter"]');
+const startBtn = document.querySelector('button[data-type="start"]');
+const details = document.getElementById("details");
 
 const myWords = [
     "mot1",
@@ -13,8 +15,20 @@ const myWords = [
     "mot10"
 ];
 
+// console.log();
+
 // Renvoie un entier aléatoire dans l'intervalle passé en paramètre
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+startBtn.addEventListener('click', () => {
+    
+    let gameWord = myWords[getRandomInt(myWords.length-1)];
+    
+    for (let i = 0; i < gameWord.length; i++) {
+        let mySpan = document.createElement("span");
+        mySpan.innerHTML = " _ ";
+        details.append(mySpan);
+    }
+});
