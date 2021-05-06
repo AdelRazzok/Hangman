9e19c5723cde8fbef6d68a77d115d72d24bb8a93
Delino <64547069+d3lino@users.noreply.github.com>
2021-05-06 14:21:12 +0100

    Mise en place de la forme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
+const keybord = document.querySelectorAll('button[data-type="letter"]');
+
 const myWords = [
     "mot1",
     "mot2",
     "mot3",
     "mot4",
-    "mot5"
+    "mot5",
+    "mot6",
+    "mot7",
+    "mot8",
+    "mot9",
+    "mot10"
 ];
+
+// Renvoie un entier aléatoire dans l'intervalle passé en paramètre
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
