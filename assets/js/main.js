de32772a7b93d8c43cc9fa7e7efe2db9874fdcad
Delino <64547069+d3lino@users.noreply.github.com>
2021-05-07 09:57:32 +0100

    Ajout du bouton recommencer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const keybord = document.querySelectorAll('button[data-type="letter"]');
 const startBtn = document.querySelector('button[data-type="start"]');
+const resetBtn = document.querySelector('button[data-type="reset"]');
 const details = document.getElementById("details");
 const myWords = [
     "PREMIER",
@@ -15,6 +16,7 @@ const myWords = [
 ];
 
 var nbTry = 7;
+var score = 0;
 
 // Renvoie un entier aléatoire dans l'intervalle passé en paramètre
 function getRandomInt(max) {
@@ -25,6 +27,9 @@ tryCount.innerHTML = nbTry;
 
 startBtn.addEventListener('click', () => {
     
+    startBtn.disabled = true;
+    resetBtn.disabled = false;
+
     // Récupère un mot du tableau au hasard
     let gameWord = myWords[getRandomInt(myWords.length-1)];
     
@@ -49,10 +54,11 @@ startBtn.addEventListener('click', () => {
 
             // Compare l'entrée clavier à chaque lettre du mot
             for (let i = 0; i < spanTab.length; i++) {
-
+                
                 if (this.dataset.value == spanTab[i].getAttribute("id")) {
                     isGood = true;
                     spanTab[i].innerHTML = this.dataset.value;
+                    score++;
                 }
                 this.style.backgroundColor = "black";
             }
@@ -60,12 +66,43 @@ startBtn.addEventListener('click', () => {
                 nbTry --;
                 tryCount.innerHTML = nbTry;
             }
-
-            
-
+            if (score >= spanTab.length) {
+                alert("Vous avez gagné !");
+            }
             if (nbTry <= 0) {
                 tryCount.innerHTML = "PERDU";
             }
+            console.log("Score : " + score);
+            console.log("Essais restants : " + nbTry);
         });
+    });
+
+    // Relance une partie
+    resetBtn.addEventListener('click', () => {
+    
+        // On réinitialise nos paramètres
+        nbTry = 7;
+        score = 0;
+        tryCount.innerHTML = nbTry;
+        details.innerHTML = "";
+
+        keybord.forEach(element => {
+            element.style.backgroundColor = "rgb(95, 158, 160)";
+        });
+
+        // Récupère un mot du tableau au hasard
+        let gameWord = myWords[getRandomInt(myWords.length-1)];
+        
+        // Transforme le mot en tableau de caractères
+        let cutGameWord = gameWord.split('');
+        
+        // Parcourir le tableau et créer un span pour chaque lettre
+        for (let i = 0; i < cutGameWord.length; i++) {
+            let mySpan = document.createElement("span");
+
+            mySpan.setAttribute("id", cutGameWord[i]);
+            mySpan.innerHTML = " _ ";
+            details.append(mySpan);
+        }
     });
 });
