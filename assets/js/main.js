7aeea65914a9add55fcf2fcb250680dac3a205b9
Delino <64547069+d3lino@users.noreply.github.com>
2021-05-07 16:59:56 +0100

    debut animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,12 @@ const resetBtn = document.querySelector('button[data-type="reset"]');
 
 const details = document.getElementById("details");
 const myWords = [
-    "PREMIER",
-    "DEUXIEME",
-    "TROISIEME",
-    "QUATRIEME",
-    "CINQUIEME",
-    "SIXIEME",
-    "SEPTIEME",
-    "HUITIEME",
-    "NEUVIEME",
-    "DIXIEME"
+    "COURT",
+    "COURT",
+    "COURT",
+    "LOOOOONG",
+    "LOOOOONG",
+    "LOOOOONG"
 ];
 
 var nbTry = 7;
@@ -25,13 +21,13 @@ function getRandomInt(max) {
 }
 
 startBtn.addEventListener('click', () => {
-    
+
     startBtn.disabled = true;
     resetBtn.disabled = false;
 
     // Récupère un mot du tableau au hasard
-    let gameWord = myWords[getRandomInt(myWords.length-1)];
-    
+    let gameWord = myWords[getRandomInt(myWords.length - 1)];
+
     // Transforme le mot en tableau de caractères
     let cutGameWord = gameWord.split('');
     let nbChar = 0;
@@ -49,7 +45,7 @@ startBtn.addEventListener('click', () => {
 
     // Découpe l'écran en autant de parties que de caractères
     let a = 0;
-    for (let i = nbChar; i > 0; i--) {
+    for (let i = nbChar; i >= 0; i--) {
         let myDiv = document.createElement("div");
         myDiv.setAttribute("id", "div" + a);
         myDiv.setAttribute("class", "cut-div");
@@ -58,13 +54,9 @@ startBtn.addEventListener('click', () => {
         a++;
     }
 
-    // Et on ajoute une case en plus pour la victoire
-    let myDiv = document.createElement("div");
-        myDiv.setAttribute("id", "div" + a);
-        myDiv.setAttribute("class", "cut-div");
-        cutScreen.append(myDiv);
-
+    // On récupère nos fragments d'écran pour les utiliser
     const myCutDivs = document.querySelectorAll('div[class="cut-div"]');
+
     myCutDivs.forEach(element => {
         element.style.width = "500px";
         element.style.border = "1px solid black";
@@ -75,6 +67,13 @@ startBtn.addEventListener('click', () => {
     createPerso.setAttribute("id", "perso");
     createPerso.setAttribute("class", "perso");
 
+    let imagePerso = document.createElement("img");
+    imagePerso.setAttribute("src", "assets/img/perso.png");
+    imagePerso.setAttribute("alt", "Perso");
+    imagePerso.setAttribute("class", "perso-img");
+
+    createPerso.appendChild(imagePerso);
+
     let createHeart = document.createElement("img");
     createHeart.setAttribute("src", "assets/img/heart.png");
     createHeart.setAttribute("alt", "Heart");
@@ -84,82 +83,83 @@ startBtn.addEventListener('click', () => {
     createLife.setAttribute("id", "tryCount");
     createLife.setAttribute("class", "life");
 
+    let createTrophy = document.createElement("img");
+    createTrophy.setAttribute("src", "assets/img/chest.png");
+    createTrophy.setAttribute("alt", "Chest");
+    createTrophy.setAttribute("id", "chest");
+    createTrophy.setAttribute("class", "chest");
+
     myCutDivs[0].append(createHeart);
     myCutDivs[0].append(createLife);
-    myCutDivs[0].append(createPerso); 
+    myCutDivs[0].append(createPerso);
+    myCutDivs[myCutDivs.length - 1].append(createTrophy);
 
     tryCount.innerHTML = nbTry;
 
     // A chaque appui sur une lettre :
     keybord.forEach(element => {
-        element.addEventListener('click', function() {
-            
+        element.addEventListener('click', function () {
+
             // Récuperer nos span en tableau
             let spanTab = document.querySelectorAll("span");
             let isGood = false;
 
             // Compare l'entrée clavier à chaque lettre du mot
             for (let i = 0; i < spanTab.length; i++) {
-                
+
                 if (this.dataset.value == spanTab[i].getAttribute("id")) {
                     isGood = true;
                     spanTab[i].innerHTML = this.dataset.value;
+                    imagePerso.src = "assets/img/perso.png";
 
                     score++;
                 }
                 this.disabled = true;
             }
             if (!isGood) {
-                nbTry --;
+                nbTry--;
                 tryCount.innerHTML = nbTry;
-            }else {
+
+                let createRock = document.createElement("img");
+                createRock.setAttribute("src", "assets/img/rock.png");
+                createRock.setAttribute("alt", "Rock");
+                createRock.setAttribute("class", "rock");
+                myCutDivs[score].append(createRock);
+
+                setTimeout(function() {
+                    createRock.remove();
+                    imagePerso.src = "assets/img/fail_perso.png";
+                }, 1000);
+            } else {
                 myCutDivs[score].append(createPerso);
             }
 
+            // En cas de victoire
             if (score >= spanTab.length - 1) {
-                alert("Vous avez gagné !");
+
                 keybord.forEach(element => {
                     element.disabled = true;
                 });
+
+                chest.src = "assets/img/open_chest.png";
+                chest.style.width = "50%";
+                chest.style.bottom = "50%";
+                chest.style.left = "25%";
+
+                imagePerso.src = "assets/img/win_perso.png";
             }
-            if (nbTry <= 0) {
-                alert("Vous avez perdu !");
+
+            // En cas de défaite
+            if (nbTry == 0) {
                 keybord.forEach(element => {
                     element.disabled = true;
                 });
+                imagePerso.src = "assets/img/death_perso.png";
             }
         });
     });
+});
 
-    // Relance une partie
-    resetBtn.addEventListener('click', () => {
-    
-        // Replace le personnage sur la première case
-        myCutDivs[0].append(createPerso);
-        
-        // On réinitialise nos paramètres
-        nbTry = 7;
-        score = 0;
-        tryCount.innerHTML = nbTry;
-        details.innerHTML = "";
-
-        keybord.forEach(element => {
-            element.disabled = false;
-        });
-
-        // Récupère un mot du tableau au hasard
-        let gameWord = myWords[getRandomInt(myWords.length-1)];
-        
-        // Transforme le mot en tableau de caractères
-        let cutGameWord = gameWord.split('');
-        
-        // Parcourir le tableau et créer un span pour chaque lettre
-        for (let i = 0; i < cutGameWord.length; i++) {
-            let mySpan = document.createElement("span");
-
-            mySpan.setAttribute("id", cutGameWord[i]);
-            mySpan.innerHTML = " _ ";
-            details.append(mySpan);
-        }
-    });
+resetBtn.addEventListener("click", () => {
+    document.location.reload();
 });
